ec5a5033a2f7c258074692637c1d14c7a7a12c06
oguzhannx <oguzhanoztr00@gmail.com>
2021-12-18 17:30:57 +0300

    .

---
+++ Entities/Concrete/User.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    class User : IEntity
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Surname{ get; set; }
+        public DateTime BirthDate { get; set; }
+        public String BloodType { get; set; }
+
+    }
+}
