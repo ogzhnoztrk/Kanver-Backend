ed83d3da65e499c720d7599b88af411e7ab39662
oguzhanoztrk00@gmail.com <oguzhanoztrk00@gmail.com>
2022-01-30 14:41:55 +0300

    about Değişti

--- Entities/Concrete/About.cs
+++ Entities/Concrete/About.cs
@@ -1,12 +1,13 @@
-﻿using Entities.Abstract;
+﻿using System;
+using Entities.Abstract;
 
 namespace Entities.Concrete
 {
     public class About : IEntity
     {
         public int AboutId { get; set; }
-        public string Content1 { get; set; }
-        public string Content2 { get; set; }
-        public string Content3 { get; set; }
+
+        public string Head{ get; set; }
+        public string Content { get; set; }
     }
 }
